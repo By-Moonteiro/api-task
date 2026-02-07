637e29f8702e1c05f25567110d9142ab89fc2273
Moonteiro <owagnerlucas0@gmail.com>
2026-02-07 15:14:35 -0300

    feat: add error handling middleware

---
+++ src/middlewares/error-handler.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "../errors/app-error";
+
+export const errorHandler = (
+  err: Error,
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      message: err.message,
+    });
+  }
+
+  return res.status(500).json({
+    message: "Error interno no servidor",
+  });
+};
